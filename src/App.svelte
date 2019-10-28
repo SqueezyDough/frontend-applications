f75148ad3c76a5fd9c96be5cdc9f82d45ea071bc
SqueezyDough <lvanbiljouw@hotmail.com>
2019-10-28 15:30:25 +0100

    :sparkles: Add statue component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,23 @@
 <script>
-    import Statue from './components/Statue.svelte';
+    import {onMount} from 'svelte';
+    import results from '../src/data/statues.js';
+    import Statue from './components/statue.svelte'
+
+    let data = [];
+
+    onMount( async () => {
+        data = await results()
+    })
 </script>
-<style>
-</style>
-<Statue data=Data/>
+
+<ul>
+    {#each data as Statue}
+        <li>
+            <Statue {Statue}/>
+        </li>
+
+        {:else}
+
+        Loading...
+    {/each}
+</ul>
--- src/components/Statue.svelte
+++ src/components/Statue.svelte
@@ -1,3 +1,13 @@
 <script>
-
+    export let Statue;
 </script>
+
+<style>
+img {
+    display: block;
+    height: 200px;
+}
+</style>
+
+{Statue.title.value}
+<img src={Statue.image.value} alt={Statue.title.value}>
